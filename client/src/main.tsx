a65ad5fca5baffaa186faf3713c9e49b8aca12ab
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-04-06 16:38:17 +0200

    refactor: moved essential styles to main component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -16,7 +16,9 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY!}>
-      <RouterProvider router={router} />
+      <div className="font-inter min-h-screen">
+        <RouterProvider router={router} />
+      </div>
     </ClerkProvider>
   </StrictMode>,
 );
