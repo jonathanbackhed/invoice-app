b6c3e0ff2b26c7b43b0a04ce0cbc3fcadfc7655b
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2026-04-29 21:23:29 +0200

    feat: add tabs for signed in

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -6,19 +6,8 @@ export default function Navbar() {
   return (
     <header className="sticky top-4 mx-auto mb-4 flex w-full max-w-4xl justify-between rounded-full border border-neutral-500 bg-white px-6 py-3 shadow-md">
       <h1 className="text-lg font-black tracking-tight text-indigo-500 uppercase">Invoice app</h1>
-      <div className="flex items-center justify-center space-x-2">
-        {/* <nav className="flex items-center justify-center space-x-2">
-            <NavLink to="/" className="font-semibold tracking-tight hover:cursor-pointer hover:text-indigo-500">
-              Priser
-            </NavLink>
-            <NavLink to="/" className="font-semibold tracking-tight hover:cursor-pointer hover:text-indigo-500">
-              FAQ
-            </NavLink>
-            <NavLink to="/" className="font-semibold tracking-tight hover:cursor-pointer hover:text-indigo-500">
-              Varför oss?
-            </NavLink>
-          </nav> */}
-        <Show when="signed-out">
+      <Show when="signed-out">
+        <div className="flex items-center space-x-2">
           <SignInButton>
             <button className="font-semibold tracking-tight hover:cursor-pointer hover:text-indigo-500">
               Logga in
@@ -29,11 +18,17 @@ export default function Navbar() {
               Skapa konto
             </button>
           </SignUpButton>
-        </Show>
-        <Show when="signed-in">
-          <UserButton showName />
-        </Show>
-      </div>
+        </div>
+      </Show>
+      <Show when="signed-in">
+        <nav className="hidden items-center space-x-2 tracking-tight sm:flex">
+          <NavLink to="/dashboard">Dashboard</NavLink>
+          <NavLink to="clients">Kunder</NavLink>
+          <NavLink to="services">Tjänster/Produkter</NavLink>
+          <NavLink to="profile">Profil</NavLink>
+        </nav>
+        <UserButton showName />
+      </Show>
     </header>
   );
 }
